acb0ea90a773d52049290515a18775ea54d8f926
Almira Khafizova <mimirka80@gmail.com>
2026-06-12 14:31:51 +0200

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Almira Khafizova on 12.06.26.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple-Frameworks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple-Frameworks
+//
+//  Created by Almira Khafizova on 12.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
